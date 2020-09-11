fbe7fe5ede7051cf68b4158c0a6407fad7f7913b
Danica Fisher <dlwylin@gmail.com>
2020-09-11 07:47:35 -0700

    Adds dad and ron components

---
+++ client/src/Dad.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Dad = function(props) {
+  return (
+  <h4>{props.joke}</h4>
+  )
+}
+
+export default Dad;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import axios from 'axios';
+import Dad from './Dad.jsx';
+import Ron from './Ron.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dad: 'Dad joke',
+      ron: 'Ron joke'
+    };
+
+  }
 
   render() {
+    const curr = this;
     return (
-      <h1>JokeMail</h1>
+      <div>
+        <div>
+          <h1>JokeMail</h1>
+        </div>
+        <Dad joke={curr.state.dad}/>
+        <Ron joke={curr.state.ron}/>
+      </div>
+
+
+
+
     )
   }
 };
